d0a5fd0421e30f570a0d5375b293328199ef982b
Arthur Villard <arthur.villard@epfl.ch>
2017-11-20 17:51:55 +0100

    Completed client's proof implementation End of client's implementation

--- dagapython/client.go
+++ dagapython/client.go
@@ -15,7 +15,7 @@ type Client struct {
 }
 
 /*CreateRequest generates the elements for the authentication request (T0, S) and the generation of the client's proof (s)*/
-func (client *Client) CreateRequest(context ContextEd25519) (T0 abstract.Point, S []abstract.Point) {
+func (client *Client) CreateRequest(context ContextEd25519) (T0 abstract.Point, S []abstract.Point, s abstract.Scalar) {
 	//Step 1: generate ephemeral DH keys
 	z := context.C.Scalar().Pick(random.Stream)
 	Z := context.C.Point().Mul(nil, z)
@@ -47,6 +47,7 @@ func (client *Client) CreateRequest(context ContextEd25519) (T0 abstract.Point, 
 		S[i] = context.C.Point().Mul(nil, exp)
 		exp.Mul(exp, context.C.Scalar().SetBytes(shared[i]))
 	}
+	s = exp
 
 	//Add the client's ephemeral public key to the commitments
 	/*Prepend taken from comment at
@@ -55,11 +56,11 @@ func (client *Client) CreateRequest(context ContextEd25519) (T0 abstract.Point, 
 	copy(S[1:], S)
 	S[0] = Z
 
-	return T0, S
+	return T0, S, s
 }
 
 //GenerateProofCommitments creates and returns the client's commitments t and the random wieghts w
-func (client *Client) GenerateProofCommitments(context ContextEd25519, T0, Sm abstract.Point) (t []abstract.Point, w []abstract.Scalar) {
+func (client *Client) GenerateProofCommitments(context ContextEd25519, T0 abstract.Point, s abstract.Scalar) (t []abstract.Point, v, w []abstract.Scalar) {
 	//Generates w randomly except for w[client.index] = 0
 	w = make([]abstract.Scalar, len(context.H))
 	for i := range w {
@@ -68,10 +69,11 @@ func (client *Client) GenerateProofCommitments(context ContextEd25519, T0, Sm ab
 	w[client.index] = context.C.Scalar().Zero()
 
 	//Generates random v (2 per client)
-	v := make([]abstract.Scalar, 2*len(context.H))
+	v = make([]abstract.Scalar, 2*len(context.H))
 	for i := 0; i < len(v); i++ {
 		v[i] = context.C.Scalar().Pick(random.Stream)
 	}
+
 	//Generates the commitments t (3 per clients)
 	t = make([]abstract.Point, 3*len(context.H))
 	for i := 0; i < len(context.H); i++ {
@@ -79,6 +81,7 @@ func (client *Client) GenerateProofCommitments(context ContextEd25519, T0, Sm ab
 		b := context.C.Point().Mul(nil, v[2*i])
 		t[3*i] = context.C.Point().Add(a, b)
 
+		Sm := context.C.Point().Mul(nil, s)
 		c := context.C.Point().Mul(Sm, w[i])
 		d := context.C.Point().Mul(nil, v[2*i+1])
 		t[3*i+1] = context.C.Point().Add(c, d)
@@ -88,11 +91,26 @@ func (client *Client) GenerateProofCommitments(context ContextEd25519, T0, Sm ab
 		t[3*i+2] = context.C.Point().Add(e, f)
 	}
 
-	return t, w
+	return t, v, w
 }
 
 //GenerateProofResponses creates the responses to the challenge cs sent by the servers
-// TODO:
-func (client *Client) GenerateProofResponses(context ContextEd25519, s abstract.Scalar, cs abstract.Scalar, t []abstract.Point, w []abstract.Scalar) (c, r []abstract.Scalar) {
-	return nil, nil
+func (client *Client) GenerateProofResponses(context ContextEd25519, s, cs abstract.Scalar, w, v []abstract.Scalar) (c, r []abstract.Scalar) {
+	//Generates the c array
+	copy(c, w)
+	sum := context.C.Scalar().Zero()
+	for _, i := range w {
+		sum = context.C.Scalar().Add(sum, i)
+	}
+	c[client.index] = context.C.Scalar().Sub(cs, sum)
+
+	//Generates the responses
+	copy(r, v)
+	a := context.C.Scalar().Mul(c[client.index], client.Private)
+	r[2*client.index] = context.C.Scalar().Sub(v[2*client.index], a)
+
+	b := context.C.Scalar().Mul(c[client.index], s)
+	r[2*client.index+1] = context.C.Scalar().Sub(v[2*client.index+1], b)
+
+	return c, r
 }
