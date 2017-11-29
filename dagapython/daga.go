bc078d83163698db54ceb1fa26d4ea4bfac2279d
Arthur Villard <arthur.villard@epfl.ch>
2017-11-29 14:22:29 +0100

    Refactored common daga file

--- dagapython/daga.go
+++ dagapython/daga.go
@@ -1,7 +1,7 @@
 package dagapython
 
 import (
-	"crypto/rand"
+	"fmt"
 
 	"gopkg.in/dedis/crypto.v0/abstract"
 	"gopkg.in/dedis/crypto.v0/ed25519"
@@ -25,46 +25,19 @@ type ContextEd25519 struct {
 	C ed25519.Curve
 }
 
-/*GenerateRandomBytes returns securely generated random bytes.
-It will return an error if the system's secure random
-number generator fails to function correctly, in which
-case the caller should not continue.
-https://elithrar.github.io/article/generating-secure-random-numbers-crypto-rand/ */
-func GenerateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
-}
+var suite = ed25519.NewAES128SHA256Ed25519(false)
 
-/*RandReader struct used to implement the io.Reader interface for GenerateRandomBytes*/
-type RandReader struct{}
-
-/*Read function for GenerateRandomBytes*/
-func (r RandReader) Read(b []byte) (int, error) {
-	b, err := GenerateRandomBytes(len(b))
-	if err != nil {
-		return 0, err
-	}
-	return len(b), nil
-}
-
-/*Sign uses gnerates a Schnorr signature*/
+/*Sign gnerates a Schnorr signature*/
 func Sign(priv abstract.Scalar, msg []byte) (s []byte, err error) {
-	suite := ed25519.NewAES128SHA256Ed25519(false)
 	s, err = sign.Schnorr(suite, priv, msg)
 	if err != nil {
-		panic("Error in the signature generation")
+		return nil, fmt.Errorf("Error in the signature generation")
 	}
-	return s, err
+	return s, nil
 }
 
-/*Verify check if a Schnorr signature is valid*/
+/*Verify checks if a Schnorr signature is valid*/
 func Verify(public abstract.Point, msg, sig []byte) (err error) {
-	suite := ed25519.NewAES128SHA256Ed25519(false)
 	err = sign.VerifySchnorr(suite, public, msg, sig)
 	return err
 }
